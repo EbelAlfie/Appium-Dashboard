e03e5e4344a10902f99c47f2cd22f80c2587dbaf
Davis Inde Satya <davis.onde.wei@gmail.com>
2024-11-30 20:51:05 +0700

    added uistate base

--- app/component/DeviceList.tsx
+++ app/component/DeviceList.tsx
@@ -4,30 +4,48 @@ import { useEffect, useMemo, useState } from "react"
 import { DeviceItem } from "./DeviceItem"
 
 type DeviceListProps = {
-    devices: DeviceModel[],
-    setDevices: (devices: DeviceModel[]) => void
+    devices: DeviceListUiState,
+    setDevices: (devices: DeviceListUiState) => void
     onUseDevice: (device: DeviceModel) => void
 }
+
+type Loading = { type:"loading" }
+type Empty = { type:"empty" } 
+type Success = { type: "success", data: DeviceModel[] }
+type Failed = { type: "failed", error: Error }
+
+export type DeviceListUiState = Loading|Empty|Success|Failed
 
 export const DeviceList = (props: DeviceListProps) => {
 
     useEffect(() => {
         appiumService.getDevices()
         .then((result) => {
-            props.setDevices(result)
+            if (result.length <= 0) props.setDevices({type: 'empty'});
+            else props.setDevices({ type: "success", data: result })
         }).catch(error => {
-
+            props.setDevices({ type: "failed", error: error })
         })
     }, [])
 
     const deviceItems = useMemo(() => {
-        return props.devices?.map((items) => 
-            <DeviceItem 
-                key={items.udid} 
-                device={items}
-                onUseButtonClicked={props.onUseDevice}
-            />
-        )
+        const deviceUiState = props.devices
+        switch(deviceUiState.type) {
+            case 'loading':
+                return <p>loading cuy</p>
+            case 'success':
+                return deviceUiState?.data.map((items) => 
+                    <DeviceItem 
+                        key={items.udid} 
+                        device={items}
+                        onUseButtonClicked={props.onUseDevice}
+                    />
+                )
+            case 'empty':
+                return <p>Empty</p>
+            default:
+                return <p>Hehe</p>
+        }
     }, [props.devices])
 
     return (
--- app/component/DeviceManager.tsx
+++ app/component/DeviceManager.tsx
@@ -1,11 +1,11 @@
 import { DeviceModel } from "@/domain/Device"
-import { DeviceList } from "./DeviceList"
+import { DeviceList, DeviceListUiState } from "./DeviceList"
 import { Devices } from "./DeviceMirrors"
 import { useState } from "react"
 import { appiumService } from "@/api/AppiumService"
 
 export const DevicesManager = () => {
-    const [devices, setDevices] = useState<DeviceModel[]>(null!)
+    const [devices, setDevices] = useState<DeviceListUiState>({ type: 'loading' })
     const [deviceMirrors, addMirror] = useState<DeviceModel[]>([])
 
     const useDevice = (device: DeviceModel) => {
